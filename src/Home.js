0c43188acc1e1eb91e20eb4098eafc71ca497f2e
sohamdutta <xzoham@gmail.com>
2020-10-28 17:16:52 -0400

    feat: conditional rendering for home page finished

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import Content from './Content.js'
 
-class Home extends React.Component {
+export default class Home extends React.Component {
   state = {
     started: false,
-    complete: false
+    completed: false
   }
+
+
   render() {
     return (
-      (this.state.started) ? (<div>
-        <Content />
-      </div >) : (<div>
-        <button onClick="started">Start</button>
-      </div>)
-  }
+      <div>
+        {this.state.started ? (<div className="container center">>
+          <Content />
+          <button onClick={() => this.setState({ started: false })}>Reset</button>
+        </div>) :
+          (<div className="container center">
+            <button onClick={() => this.setState({ started: true })} className="btn center" >Start</button>
+          </div>)}
+      </div>
     )
+  }
 }
-
-
-export default Home
