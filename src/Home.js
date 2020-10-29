9a5b4bb2f84486f5d90cc35c8be4acb602f97a8f
sohamdutta <xzoham@gmail.com>
2020-10-29 12:40:13 -0400

    feat:state changes on answer selection

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Questions from './data/questions.json'
 import { groupAnswers, shuffleData } from './HelperFunctions.js'
 
 export default class Home extends React.Component {
-  state = {
-    started: false,
-    completed: false,
-    score: 0,
-    questionNumber: 1,
-    selected: '',
-    checked: false
+  constructor(props) {
+    super(props)
+    this.state = {
+      started: false,
+      completed: false,
+      score: 0,
+      questionNumber: 1,
+      selected: '',
+      checked: false
+    }
+    this.selectionMethod.bind(this)
   }
-
+  selectionMethod(e) {
+    this.state.selected = e.target.value
+    console.log(this.state)
+  }
   render() {
     let randomQuestions = shuffleData(Questions)
     let groupedAnswers = groupAnswers(randomQuestions[this.state.questionNumber].correct, randomQuestions[this.state.questionNumber].incorrect)
+
 
     return (
       <div>
@@ -30,11 +38,14 @@ export default class Home extends React.Component {
                 <div>
                   <ul>
                     {groupedAnswers.map(answer => (
-                      <div>
-                        <li key={answer}>
+                      <div key={answer}>
+                        <li>
                           <input
                             type="checkbox"
                             id={answer}
+                            value={answer}
+                            onChange={(event) => this.selectionMethod(event)}
+
                           />
                           <label htmlFor={answer}>{answer}</label>
                         </li>
@@ -46,7 +57,7 @@ export default class Home extends React.Component {
 
               <button className='btn' onClick={() => this.setState({ started: false, completed: false, score: 0, questionNumber: 1 })}>Reset</button>
 
-              <button className='btn'>Sumbit</button>
+              <button className='btn' >Submit</button>
 
             </div>
 
