597b04437b4759eeac640fe41cd2e8d74fea088c
sohamdutta <xzoham@gmail.com>
2020-10-29 14:56:55 -0400

    feat: testing

--- src/Home.js
+++ src/Home.js
@@ -15,10 +15,14 @@ export default class Home extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this)
   }
   selectionMethod(e) {
-    console.log('before', this.state)
-    this.state.selected = e.target.value
-    console.log('after', this.state)
+    if (this.state.selected === e.target.value) {
+      this.state.selected = ''
+    } else {
+      this.state.selected = e.target.value
+    }
+    console.log(this.state)
   }
+
   handleSubmit(selected, correct) {
     if (selected === correct) {
       this.setState({ score: this.state.score + 1, selected: '', questionNumber: this.state.questionNumber + 1 })
@@ -28,58 +32,65 @@ export default class Home extends React.Component {
     }
     console.log('submit', this.state)
   }
+
   render() {
+
     let randomQuestions = this.state.first10
     let groupedAnswers = groupAnswers(randomQuestions[this.state.questionNumber].correct, randomQuestions[this.state.questionNumber].incorrect)
 
-
     return (
       <div>
-        {(this.state.questionNumber < 11) ?
+        {
+          (this.state.questionNumber < 11) ?
+            (
+              <div className=" container center">
+                <div className="card">
+                  <h3>{this.state.score}/10</h3>
+                  <div>
+                    <p>Question {this.state.questionNumber}</p> {randomQuestions[this.state.questionNumber].question}
+                  </div>
+                  <div>
+                    <ul>
+                      {groupedAnswers.map(answer => (
+                        <div key={answer}>
+                          <li>
+                            <input
+                              type="checkbox"
+                              id={answer}
+                              value={answer}
+                              onChange={(event) => this.selectionMethod(event)}
+                            />
+                            <label htmlFor={answer}>{answer}</label>
+                          </li>
+                        </div>
+                      )
+                      )
+                      }
+                    </ul>
+                  </div>
+                </div>
 
-          (
-            <div className="container center">
-              <div>
-                <h3>{this.state.score}/10</h3>
-                <div>
-                  <p>{this.state.questionNumber}.</p> {randomQuestions[this.state.questionNumber].question}
-                </div>
-                <div>
-                  <ul>
-                    {groupedAnswers.map(answer => (
-                      <div key={answer}>
-                        <li>
-                          <input
-                            className='checkControl'
-                            type="checkbox"
-                            id={answer}
-                            value={answer}
-                            onChange={(event) => this.selectionMethod(event)}
-                          />
-                          <label htmlFor={answer}>{answer}</label>
-                        </li>
-                      </div>
-                    ))}
-                  </ul>
-                </div>
+                <button className='btn' onClick={() => this.setState({ started: false, completed: false, score: 0, questionNumber: 1, first10: shuffleData(Questions) })}>Reset</button>
+
+                <button className='btn' onClick={() => this.handleSubmit(this.state.selected, randomQuestions[this.state.questionNumber].correct)}>
+                  Submit
+                </button>
+
               </div>
+            )
+            :
+            (
+              <div className="container center">
 
-              <button className='btn' onClick={() => this.setState({ started: false, completed: false, score: 0, questionNumber: 1 })}>Reset</button>
+                <h1>Your Score is {this.state.score}!</h1>
 
-              <button className='btn' onClick={() => this.handleSubmit(this.state.selected, randomQuestions[this.state.questionNumber].correct)} >Submit</button>
+                <button className="btn"
+                  onClick={() => this.setState({ score: 0, questionNumber: 1, selected: '', first10: shuffleData(Questions) })}
+                > Try Again
+              </button>
 
-            </div>
-
-          )
-          :
-          (
-            <div className="container center">
-              <h1>Your Score is {this.state.score}!</h1>
-              <button className="btn"
-                onClick={() => this.setState({ score: 0, questionNumber: 1, selected: '', first10: shuffleData(Questions) })}
-              > Try Again</button>
-            </div>
-          )}
+              </div>
+            )}
       </div>
     )
   }
