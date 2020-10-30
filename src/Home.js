3a71df2e687e0b85cfcf4ce31501b8b25ae7258b
sohamdutta <xzoham@gmail.com>
2020-10-29 21:59:52 -0400

    finished MVP

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Questions from './data/questions.json'
 import { groupAnswers, shuffleData } from './HelperFunctions.js'
 
-
 export default class Home extends React.Component {
   constructor(props) {
     super(props)
@@ -11,11 +10,14 @@ export default class Home extends React.Component {
       score: 0,
       questionNumber: 1,
       selected: '',
+      shuffle: true,
+      shuffledAnswers: []
+
     }
     this.selectionMethod = this.selectionMethod.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleShuffle = this.handleShuffle.bind(this)
   }
-
   selectionMethod(e) {
     if (this.state.selected === e.target.value) {
       this.setState({ selected: '' })
@@ -27,17 +29,26 @@ export default class Home extends React.Component {
 
   handleSubmit(selected, correct) {
     if (selected === correct) {
-      this.setState({ score: this.state.score + 1, selected: '', questionNumber: this.state.questionNumber + 1 })
+      this.setState({ score: this.state.score + 1, selected: '', questionNumber: this.state.questionNumber + 1, shuffle: true })
     } else {
       alert(`Sorry! the correct answer is ${correct}`)
-      this.setState({ selected: '', questionNumber: this.state.questionNumber + 1 })
+      this.setState({ selected: '', questionNumber: this.state.questionNumber + 1, shuffle: true })
     }
     console.log('submit', this.state)
+  }
+  // handle shuffle needs to shuffle each round
+  handleShuffle(arr) {
+    if (this.state.shuffle === true) {
+      this.state.shuffle = false
+      this.state.shuffledAnswers = shuffleData(arr)
+    }
+    return this.state.shuffledAnswers
   }
 
   render() {
     let randomQuestions = this.state.first10
     let groupedAnswers = groupAnswers(randomQuestions[this.state.questionNumber].correct, randomQuestions[this.state.questionNumber].incorrect)
+    let answers = this.handleShuffle(groupedAnswers)
     return (
       <div>
         {
@@ -45,13 +56,13 @@ export default class Home extends React.Component {
             (
               <div className=" container center">
                 <div className="card">
-                  <h3>{this.state.score}/10</h3>
-                  <div>
+                  <h4>{this.state.score}/10</h4>
+                  <h5>
                     <p>Question {this.state.questionNumber}</p> {randomQuestions[this.state.questionNumber].question}
-                  </div>
+                  </h5>
                   <div>
                     <ul>
-                      {groupedAnswers.map(answer => (
+                      {answers.map(answer => (
                         <div key={answer}>
                           <li>
                             <input
@@ -72,9 +83,9 @@ export default class Home extends React.Component {
                   </div>
                 </div>
 
-                <button className='btn' onClick={() => this.setState({ started: false, completed: false, score: 0, questionNumber: 1, first10: shuffleData(Questions) })}>Reset</button>
+                <button className='btn-large' onClick={() => this.setState({ started: false, completed: false, score: 0, questionNumber: 1, first10: shuffleData(Questions) })}>Reset</button>
 
-                <button className='btn' onClick={() => this.handleSubmit(this.state.selected, randomQuestions[this.state.questionNumber].correct)}>
+                <button className='btn-large' onClick={() => this.handleSubmit(this.state.selected, randomQuestions[this.state.questionNumber].correct)}>
                   Submit
                 </button>
 
